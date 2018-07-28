b1c7403d902e92901109133908335b581ca975f6
gjmoretti <gjmoretti@gmail.com>
2018-07-28 10:41:02 -0300

    Adcionando Header

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
+        loader: 'babel',
         query: {
           presets: ['es2015'],
           plugins: ['transform-runtime']
