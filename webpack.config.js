4b337755dfe12212aced2168f6d13be61fd341e5
Gustavo <gjmoretti@gmail.com>
2018-08-05 19:44:18 -0300

    Teste com WebPack 4

--- webpack.config.js
+++ webpack.config.js
@@ -34,18 +34,6 @@ module.exports = {
        test: /\.css$/,
        use: [ 'style-loader', 'css-loader' ]
       }
-    ],	
-    loaders: [
-      { test: /\.json$/, use: 'json-loader' },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-runtime']
-        }
-      }
     ]	
   }
 }
